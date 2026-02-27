a71c0bbb466a2f70a48ca61f8b36180eea1a9cda
Srikar <srikarjanjirala92@gmail.com>
2026-02-27 10:30:18 +0530

    Configured Database

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,16 +1,4 @@
-const mysql = require("mysql2/promise");
-
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: {
-    minVersion: "TLSv1.2",
-    rejectUnauthorized: true
-  }
-});
+const pool = require("../config/database");
 
 const createUser = async (name, email, hashedPassword) => {
   const [result] = await pool.execute(
